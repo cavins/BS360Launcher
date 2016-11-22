1e0acc561f920ed1ac90d88c8283e80455112408
lijianwen <licavins@gmail.com>
2016-11-22 10:48:32 +0800

    去掉吐司

--- device3x/src/main/java/com/ljw/device3x/customview/MobileStateView.java
+++ device3x/src/main/java/com/ljw/device3x/customview/MobileStateView.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ljw.device3x.R;
 
